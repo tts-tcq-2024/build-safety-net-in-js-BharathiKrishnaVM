2413f32bde51af8935c93688c93a5d8994895066
Bharathi Krishna V M <49585360+BharathiKrishnaVM@users.noreply.github.com>
2024-09-12 22:40:50 +0530

    test cases

--- test/soundex.test.js
+++ test/soundex.test.js
@@ -2,6 +2,7 @@ const { expect } = require('chai');
 const { generateSoundex } = require('../src/soundex');
 
 describe('Soundex Algorithm', () => {
+    
     it('should handle empty strings', () => {
         expect(generateSoundex("")).to.equal("");
     });
@@ -10,5 +11,16 @@ describe('Soundex Algorithm', () => {
         expect(generateSoundex("A")).to.equal("A000");
     });
 
+    it('should handle special characters', () =>{
+         expect(generateSoundex("S@m!th")).to.equal("S530");
+    });
+
+    it('should handle Repeated characters' ()=>{
+        expect(generateSoundex("BASS")).to.equal("B120");
+    });
+
+    it('should handle string text', ()=> {
+        expect(generateSoundex('Schmidt')).to.equal('S530');
+    });
     
-});
+})
